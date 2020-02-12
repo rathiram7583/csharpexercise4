1f15628f62789806795ff2451ae7f1ebaa137615
Rathi <rathiram7583@gmail.com>
2020-02-12 15:34:54 -0700

    Added Web Files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace Csharpexercise4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           /* Console.WriteLine("Enter Your Name");
+            string name=Console.ReadLine();
+            Console.WriteLine(":)"+name+":(");*/
+             string input=GetInput();
+            input=AddSmileyToEnd(input);
+            input=AddSmileyToBeginning(input);
+             WriteName(input);
+        }
+        static string GetInput()
+        {
+            Console.WriteLine("Please Input Some text");
+            return Console.ReadLine();
+
+        }
+        static string AddSmileyToEnd( string input)
+        {
+              input=input+":)";
+              return input;
+
+        }
+        static string AddSmileyToBeginning(string input)
+        {
+              input=":("+input;
+             return input;
+             
+        }
+        static void WriteName(string input)
+        {
+            Console.WriteLine(input);
         }
     }
 }
